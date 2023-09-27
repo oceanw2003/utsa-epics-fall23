4af277e2ed5ffde1a94a0c198fcfadf673e1db57
oceanw2003 <oceanw2003@gmail.com>
2023-09-27 14:47:29 -0500

    replaced 4 led blink 2 at a time code with correct code

--- Arduino/4_LEDs_Blink_2_at_a_time/4_LEDs_Blink_2_at_a_time.ino
+++ Arduino/4_LEDs_Blink_2_at_a_time/4_LEDs_Blink_2_at_a_time.ino
@@ -1,19 +1,24 @@
 
 int led_pin1 = 13;
-int led_pin2 = 13;
-int led_pin3 = 13;
-int led_pin4 = 13;
+int led_pin2 = 12;
  int blink_duration1 = 1000;
 
  void setup()  {}
 
 void loop() {
  
-
+ pinMode (13, led_pin1);
   digitalWrite (13,HIGH);
   delay (blink_duration1);
   pinMode (13, led_pin1);
   digitalWrite (13,LOW);
+  delay (blink_duration1);
+
+  pinMode (12, led_pin1);
+  digitalWrite (12,HIGH);
+  delay (blink_duration1);
+  pinMode (12, led_pin1);
+  digitalWrite (12,LOW);
   delay (blink_duration1);
 
 }
